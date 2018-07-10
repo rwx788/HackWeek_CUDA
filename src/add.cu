e28c8a4b1be28c86ae4328e63733b0290426c6fe
Rodion Iafarov <riafarov@suse.com>
2018-07-10 15:42:32 +0200

    Init array on GPU to avoid data transfer

--- src/add.cu
+++ src/add.cu
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <math.h>
+
+
 // Kernel function to add the elements of two arrays
-__global__
-void add(int n, float *x, float *y) {
-  for (int i = blockIdx.x * blockDim.x + threadIdx.x;
-       i < n;
-       i += blockDim.x * gridDim.x)
-  {
+__global__ void add(int n, float *x, float *y) {
+  int index = threadIdx.x + blockIdx.x * blockDim.x;
+  int stride = blockDim.x * gridDim.x;
+  for (int i = index; i < n; i += stride) {
     y[i] = x[i] + y[i];
+  }
+}
+// Kernel function to initialize the elements of two arrays
+__global__ void init(int n, float *x, float *y) {
+  int index = threadIdx.x + blockIdx.x * blockDim.x;
+  int stride = blockDim.x * gridDim.x;
+  for (int i = index; i < n; i += stride) {
+    x[i] = 1.0f;
+    y[i] = 2.0f;
   }
 }
 
@@ -19,16 +28,13 @@ int main(void) {
   cudaMallocManaged(&x, N * sizeof(float));
   cudaMallocManaged(&y, N * sizeof(float));
 
-  // initialize x and y arrays on the host
-  for (int i = 0; i < N; i++) {
-    x[i] = 1.0f;
-    y[i] = 2.0f;
-  }
+  int numSMs;
+  cudaDeviceGetAttribute(&numSMs, cudaDevAttrMultiProcessorCount, 0);
+  // initialize x and y arrays on the GPU
+  init<<<32 * numSMs, 256>>>(N, x, y);
 
   // Run kernel on all elements on the GPU
-  int numSMs;  
-  cudaDeviceGetAttribute(&numSMs, cudaDevAttrMultiProcessorCount, 0);
-  add<<<32*numSMs, 256>>>(N, x, y);
+  add<<<32 * numSMs, 256>>>(N, x, y);
 
   // Wait for GPU to finish before accessing on host
   cudaDeviceSynchronize();
